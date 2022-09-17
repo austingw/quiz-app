d7dde88205ab04e9ab904af4a546a8992369e121
Austin Williams <senortinman@gmail.com>
2022-09-17 15:54:13 -0400

    Update questions state so that question data is parsed correctly. Question elements map each question with its respective answers

--- quick-quiz/src/App.jsx
+++ quick-quiz/src/App.jsx
@@ -4,45 +4,68 @@ import Start from "./components/Start";
 import testQuestions from "./testQuestions";
 
 function App() {
-  const [questions, setQuestions] = useState(testQuestions.results);
-  const [answers, setAnswers] = useState(generateAnswers());
+  const [questions, setQuestions] = useState(getQuiz(testQuestions.results));
 
-  function generateAnswers() {
-    const answerKey = questions[0].incorrect_answers.map((answer) => {
+  function getQuiz(quizQuestions) {
+    const newQuiz = [];
+    quizQuestions.forEach((data) => newQuiz.push(generateAnswers(data)));
+    return newQuiz;
+  }
+
+  function generateAnswers(data) {
+    const answerKey = data.incorrect_answers.map((answer) => {
       return {
         answer: answer,
         isTrue: false,
       };
     });
     answerKey.push({
-      answer: questions[0].correct_answer,
+      answer: data.correct_answer,
       isTrue: true,
     });
-    //This ranomizes the array order of answers so that there isn't a pattern
+    //shuffledAnswers randomizes the array order of each question's answers
+    //so that there isn't a set pattern/order of correct answers for the quiz
     const shuffledAnswers = answerKey
       .map((a) => ({ sort: Math.random(), value: a }))
       .sort((a, b) => a.sort - b.sort)
       .map((a) => a.value);
 
-    return shuffledAnswers;
+    return {
+      question: data.question,
+      answers: shuffledAnswers,
+    };
   }
 
   useEffect(() => {
-    console.log(
+    /*console.log(
       `https://opentdb.com/api.php?amount=5&category=11&type=multiple`
-    );
+    );*/
+    console.log(questions);
   }, []);
+
+  const questionElements = questions.map((question) => (
+    <Question
+      key={question.question}
+      question={question.question}
+      answer1={question.answers[0]}
+      answer2={question.answers[1]}
+      answer3={question.answers[2]}
+      answer4={question.answers[3]}
+    />
+  ));
 
   return (
     <div className="App">
-      <Question
+      {questionElements}
+
+      {/*<Question
         key={questions[0].question}
         question={questions[0].question}
         answer1={answers[0]}
         answer2={answers[1]}
         answer3={answers[2]}
         answer4={answers[3]}
-      />
+  />*/}
     </div>
   );
 }
